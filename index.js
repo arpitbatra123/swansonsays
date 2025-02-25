f985d206153ad6cde526cbb4071ab1289defb710
Arpit Batra <arpitbatra123@gmail.com>
2025-02-25 13:32:03 +0530

    update package.json to require Node.js 18 and upgrade ora dependency; refactor index.js to use fetch instead of axios

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const ora = require('ora'),
-  axios = require('axios');
+const ora = require('ora');
 
 module.exports = async () => {
   const spinner = ora('Waking up Ron!').start();
   try {
-    const quoteResponse = await axios('http://ron-swanson-quotes.herokuapp.com/v2/quotes'),
-    quoteText = quoteResponse.data[0];
+    const response = await fetch('http://ron-swanson-quotes.herokuapp.com/v2/quotes');
+    const quoteText = (await response.json())[0];
 
     spinner.stop();
     console.log(`${quoteText} - Ron Swanson`);
   } catch (e) {
     spinner.stop();
     console.error(`${e.toString()} - Definitely not Ron Swanson`);
-    process.exit(1)
+    process.exit(1);
   }
-};
+}
